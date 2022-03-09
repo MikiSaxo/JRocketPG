ef00b4559505279a37b9fbe5eb2ea73a16f873bf
SimonWalenczak <92367148+SimonWalenczak@users.noreply.github.com>
2022-03-09 18:32:02 +0100

    patrouille IA nav

---
+++ Assets/Scripts/Simon/Patroller.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patroller : MonoBehaviour
+{
+    public Transform[] waypoints;
+    public int speed;
+
+    private int wayPointIndex;
+    private float dist;
+
+    void Start()
+    {
+        wayPointIndex = 0;
+        transform.LookAt(waypoints[wayPointIndex].position);
+    }
+
+    
+    void Update()
+    {
+        dist = Vector3.Distance(transform.position, waypoints[wayPointIndex].position);
+        if(dist < 1f)
+        {
+            IncreaseIndex();
+        }
+        Patrol();
+    }
+
+    void Patrol()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    void IncreaseIndex()
+    {
+        wayPointIndex++;
+        if(wayPointIndex >= waypoints.Length)
+        {
+            wayPointIndex = 0;
+        }
+        transform.LookAt(waypoints[wayPointIndex].position);
+    }
+}
