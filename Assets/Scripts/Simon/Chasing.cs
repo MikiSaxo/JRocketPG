dee7927f7c57f6452b2c6e2ac8de3895e94ff713
Frostork <92367148+SimonWalenczak@users.noreply.github.com>
2022-03-09 23:55:21 +0100

    IA ennemy chasing

---
+++ Assets/Scripts/Simon/Chasing.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chasing : MonoBehaviour
+{
+    public Patroller patroller;
+    public void OnTriggerEnter(Collider player)
+    {
+        patroller.chasing = true;
+    }
+    public void OnTriggerExit(Collider player)
+    {
+        patroller.chasing = false;
+    }
+}
--- Assets/Scripts/Simon/Patroller.cs
+++ Assets/Scripts/Simon/Patroller.cs
@@ -10,21 +10,38 @@ public class Patroller : MonoBehaviour
     private int wayPointIndex;
     private float dist;
 
+    public Transform player;
+    public Collider playerCollider;
+    public bool chasing = false;
+
     void Start()
     {
         wayPointIndex = 0;
         transform.LookAt(waypoints[wayPointIndex].position);
     }
 
-    
+
     void Update()
     {
-        dist = Vector3.Distance(transform.position, waypoints[wayPointIndex].position);
-        if(dist < 1f)
+        if(chasing == false)
         {
-            IncreaseIndex();
+            speed = 15;
+            transform.LookAt(waypoints[wayPointIndex].position);
+            dist = Vector3.Distance(transform.position, waypoints[wayPointIndex].position);
+            if(dist < 1f)
+            {
+                IncreaseIndex();
+            }
+            Patrol();
         }
-        Patrol();
+
+        if(chasing == true)
+        {
+            speed = 25;
+            transform.LookAt(player.position);
+            dist = Vector3.Distance(transform.position, player.position);
+            Patrol();
+        }
     }
 
     void Patrol()
