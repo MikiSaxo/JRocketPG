156d54772922b57fb4d036f6753e75913697ee5a
SimonWalenczak <92367148+SimonWalenczak@users.noreply.github.com>
2022-03-09 15:12:21 +0100

    update nav

--- Assets/Scripts/Simon/MiniMapFollow.cs
+++ Assets/Scripts/Simon/MiniMapFollow.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowPlayerMiniMap : MonoBehaviour
+public class MiniMapFollow : MonoBehaviour
 {
     public Rigidbody player;
 
