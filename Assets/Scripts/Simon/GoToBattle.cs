231ca59ea4289e0ed483ab09994709f6aca579b4
Frostork <92367148+SimonWalenczak@users.noreply.github.com>
2022-03-26 22:07:27 +0100

    Amarrage + Switch sur scène de combat

---
+++ Assets/Scripts/Simon/GoToBattle.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToBattle : MonoBehaviour
+{
+    public Collider player;
+    private bool can_moor = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) && can_moor == true)
+        {
+            SceneManager.LoadScene("Sam");
+        }    
+    }
+    public void OnTriggerEnter(Collider player)
+    {
+        can_moor = true;
+    }
+    public void OnTriggerExit(Collider player)
+    {
+        can_moor = false;
+    }
+}
