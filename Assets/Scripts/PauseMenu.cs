dcdd6bec9bfb745ad32d4ab10eeae4f7ead58046
Frostork <92367148+SimonWalenczak@users.noreply.github.com>
2022-03-28 20:36:43 +0200

    UI pause

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject UIPause;
+
+    private bool isPaused = false;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            UIPause.SetActive(!UIPause.activeSelf);
+
+            if(isPaused == false)
+            {
+                isPaused = true;
+                Time.timeScale = 0;
+            }
+            else
+            {
+                isPaused = false;
+                Time.timeScale = 1;
+            }
+        }
+    }
+
+    public void Continue()
+    {
+        UIPause.SetActive(false);
+    }
+
+    public void Quit()
+    {
+
+#if UNITY_STANDALONE
+
+        Application.Quit();
+#endif
+
+#if UNITY_EDITOR
+
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
