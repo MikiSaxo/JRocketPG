b6473de1ead2619da114c83bd1a7338b801afe49
Frostork <92367148+SimonWalenczak@users.noreply.github.com>
2022-04-03 00:22:40 +0200

    Revert "Objet qui flotte (bateau/caisse), Upgrade prog fini"
    This reverts commit 291699bc6760ed7fb49f149d5beaa7bf6ded7a24.

--- Assets/Scripts/Navigation/Recup_object.cs
+++ Assets/Scripts/Navigation/Recup_object.cs
@@ -11,27 +11,12 @@ public class Recup_object : MonoBehaviour
 
     public bool[] IsGPEActive;
 
-    public int nbRhum;
-    public int nbLongueVue;
-    public int nbTroublon;
-
-    public int UpgradeStacks = 0;
-
     private void Awake()
     {
         if (GameData.initialized == false)
         {
             GameData.IsGPEActive = new bool[IsGPEActive.Length];
             Array.Copy(IsGPEActive, GameData.IsGPEActive, IsGPEActive.Length);
-
-            GameData.objCaisses = new GameObject[objCaisses.Length];
-            Array.Copy(objCaisses, GameData.objCaisses, objCaisses.Length);
-
-            GameData.nbRhum = nbRhum;
-            GameData.nbLongueVue = nbLongueVue;
-            GameData.nbTroublon = nbTroublon;
-
-            GameData.UpgradeStacks = UpgradeStacks;
         }
     }
 
@@ -39,30 +24,7 @@ public class Recup_object : MonoBehaviour
     {
         IsGPEActive = GameData.IsGPEActive;
 
-        objCaisses = GameData.objCaisses;
-
-        nbRhum = GameData.nbRhum;
-        nbLongueVue = GameData.nbLongueVue;
-        nbTroublon = GameData.nbTroublon;
-
-        UpgradeStacks = GameData.UpgradeStacks;
-
-        /*for (int i = 0; i < GameData.objCaisses.Length; i++)
-        {
-            if (GameData.objCaisses[i].activeSelf == false)
-                objCaisses[i].SetActive(false);
-        }*/
-
-        if (nbRhum > 0)
-            GameData.IsGPEActive[0] = true;
-        if (nbLongueVue > 0)
-            GameData.IsGPEActive[1] = true;
-        if (nbTroublon > 0)
-            GameData.IsGPEActive[2] = true;
-
-
-
-        /*if (GameData.IsGPEActive[0] == false)
+        if (GameData.IsGPEActive[0] == false)
             objCaisses[0].SetActive(true);
         else
             objCaisses[0].SetActive(false);
@@ -75,7 +37,7 @@ public class Recup_object : MonoBehaviour
         if (GameData.IsGPEActive[2] == false)
             objCaisses[2].SetActive(true);
         else
-            objCaisses[2].SetActive(false);*/
+            objCaisses[2].SetActive(false);
 
     }
     public void OnTriggerEnter(Collider other)
@@ -83,56 +45,20 @@ public class Recup_object : MonoBehaviour
         if (other.gameObject.name == "Tonneau 1")
         {
             print("caisses 1");
-            GameData.nbRhum++;
-            GameData.objCaisses[0].SetActive(false);
+            GameData.IsGPEActive[0] = true;
+            objCaisses[0].SetActive(false);
         }
         if (other.gameObject.name == "Tonneau 2")
         {
             print("caisses 2");
-            GameData.nbLongueVue++;
-            GameData.objCaisses[1].SetActive(false);
+            GameData.IsGPEActive[1] = true;
+            objCaisses[1].SetActive(false);
         }
         if (other.gameObject.name == "Tonneau 3")
         {
             print("caisses 3");
-            GameData.nbTroublon++;
-            GameData.objCaisses[2].SetActive(false);
-        }
-        if (other.gameObject.name == "Tonneau 4")
-        {
-            print("caisses 4");
-            GameData.UpgradeStacks++;
-            GameData.objCaisses[3].SetActive(false);
-        }
-        if (other.gameObject.name == "Tonneau 5")
-        {
-            print("caisses 5");
-            GameData.objCaisses[4].SetActive(false);
-        }
-        if (other.gameObject.name == "Tonneau 6")
-        {
-            print("caisses 6");
-            GameData.objCaisses[5].SetActive(false);
-        }
-        if (other.gameObject.name == "Tonneau 7")
-        {
-            print("caisses 7");
-            GameData.objCaisses[6].SetActive(false);
-        }
-        if (other.gameObject.name == "Tonneau 8")
-        {
-            print("caisses 8");
-            GameData.objCaisses[7].SetActive(false);
-        }
-        if (other.gameObject.name == "Tonneau 9")
-        {
-            print("caisses 9");
-            GameData.objCaisses[8].SetActive(false);
-        }
-        if (other.gameObject.name == "Tonneau 10")
-        {
-            print("caisses 10");
-            GameData.objCaisses[9].SetActive(false);
+            GameData.IsGPEActive[2] = true;
+            objCaisses[2].SetActive(false);
         }
     }
 }
